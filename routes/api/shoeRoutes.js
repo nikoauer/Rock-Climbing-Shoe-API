ba9db9e1b3656c2f8b3ccdb2e211216060c435e9
Niko Auer <nikoauer26@gmail.com>
2023-10-02 11:50:08 +1100

    get all shoe data working

--- routes/api/shoeRoutes.js
+++ routes/api/shoeRoutes.js
@@ -1,2 +1,14 @@
-const router = require('express').Router
-const Shoe = require
+const router = require('express').Router();
+const Shoes = require('../../models/Shoe');
+
+router.get('/', async (req, res) => {
+    try {
+        const shoeData = await Shoes.findAll();
+        res.status(200).json(shoeData);
+    } catch (error) {
+        console.log('There was an error with retrieving all the shoes', error)
+        res.status(500).json(error);
+    }
+})
+
+module.exports = router;
